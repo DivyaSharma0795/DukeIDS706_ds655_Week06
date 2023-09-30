d1bf225d4fd082dbfad848ede2d0360db22172a7
Divya Sharma <divya.sharma@duke.edu>
2023-09-30 02:55:06 +0000

    edited main,py

--- main.py
+++ main.py
@@ -5,15 +5,27 @@ ETL-Query script
 from mylib.extract import extract
 from mylib.transform_load import load
 from mylib.query import query
+import argparse
 
+parser = argparse.ArgumentParser(description="Extract, Transform, or Load data")
+parser.add_argument(
+    "--step", type=int, help="Step number to run (1 - Extract, 2 - Load, or 3 - Query)"
+)
+args = parser.parse_args()
 # Extract
-print("Extracting data...")
-extract()
+if args.step == 1:
+    print("Extracting data...")
+    extract()
 
 # Transform and load
-print("Transforming data...")
-load()
+elif args.step == 2:
+    print("Transforming data...")
+    load()
 
 # Query
-print("Querying data...")
-query()
+elif args.step == 3:
+    print("Querying data...")
+    query()
+
+else:
+    print("Invalid step number")
