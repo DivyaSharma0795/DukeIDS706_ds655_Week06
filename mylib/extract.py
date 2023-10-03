f8c7942a929ebc6b00a7b760bd2bae5ee07e1671
Divya Sharma <divya.sharma@duke.edu>
2023-10-03 17:12:44 -0400

    Update extract.py

--- mylib/extract.py
+++ mylib/extract.py
@@ -1,13 +1,12 @@
 """
-Extract a dataset from a URL like Kaggle or data.gov. JSON or CSV formats tend to work well
-
+Extract a dataset from a URL like Kaggle or data.gov. JSON or CSV formats tend to work well # noqa: E501
 food dataset
 """
 import requests
 
 
 def extract(
-    url="https://raw.githubusercontent.com/Barabasi-Lab/GroceryDB/main/data/GroceryDB_IgFPro.csv",
+    url="https://raw.githubusercontent.com/Barabasi-Lab/GroceryDB/main/data/GroceryDB_IgFPro.csv", # noqa: E501
     file_path="data/GroceryDB.csv",
 ):
     """ "Extract a url to a file path"""
