c35d9ef8ebf32d98148da61756c4b184920c028b
Divya Sharma <divya.sharma@duke.edu>
2023-10-04 01:45:52 +0000

    updated starting value for csv files with blank ID column

--- mylib/extract.py
+++ mylib/extract.py
@@ -13,12 +13,19 @@ def extract(
 ):
     """ "Extract a url to a file path"""
     with requests.get(url) as r:
+        # print(r.content[0])
+        # print(type(r.content))
+        # print(r.content.decode("utf-8")[0])
         with open(file_path, "wb") as f:
+            if r.content.decode("utf-8")[0] == ",":
+                f.write(b"id")
+            # print(r.content)
             f.write(r.content)
     return file_path
 
 
-extract(
-    "https://gist.githubusercontent.com/netj/8836201/raw/6f9306ad21398ea43cba4f7d537619d0e07d5ae3/iris.csv",
-    "data/Iris_Data.csv",
-)
+extract()
+# extract(
+#     "https://gist.githubusercontent.com/netj/8836201/raw/6f9306ad21398ea43cba4f7d537619d0e07d5ae3/iris.csv",  # noqa: E501
+#     "data/Iris_Data.csv",
+# )
