465f5a46e573cc0ef1ecf85b1bcb6106860530b6
Divya Sharma <divya.sharma@duke.edu>
2023-10-11 17:41:50 +0000

    Updated Adv query

---
+++ mylib/Advanced_Query.py
@@ -0,0 +1,28 @@
+"""Query the database"""
+
+import sqlite3
+from prettytable import PrettyTable  # For formatting
+
+
+def Iris_query(db="Iris_Data.db"):
+    """Query the database for the top 5 rows of the db table"""
+    tablename = db.split(".")[0]
+    conn = sqlite3.connect(db)
+    cursor = conn.cursor()
+    # Getting top 5 rows of the table
+    cursor.execute("SELECT variety, avg(sepal_length) as avg_sepal_length, avg(sepal_width) as avg_sepal_width FROM " + tablename + " GROUP BY 1")
+    # print("Top 5 rows of the " + tablename + " table:")
+    # print([i[0] for i in cursor.description])  # Printing Headers
+    r_all = cursor.fetchall()
+    x = PrettyTable()
+    x.field_names = [i[0] for i in cursor.description]
+    for r in r_all:
+        x.add_row(r)
+    print(x)
+    # print(cursor.fetchall())  # Printing query output
+    conn.close()
+    return "Success"
+
+
+if __name__ == "__main__":
+    Iris_query()
